1940e6eb13d2bba58591a2db7eed2550d9c2a760
uncle_bogdan <fbogdan13@gmail.com>
2021-06-09 19:26:00 +0300

    working autorization

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const path = require('path')
+const csrf = require('csurf')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoVtormetrpom = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+
+const MONGODB_URI = `mongodb://bogdan:gxd75VdKa5UbwyVF@cluster0-shard-00-00.unmsp.mongodb.net:27017,cluster0-shard-00-01.unmsp.mongodb.net:27017,cluster0-shard-00-02.unmsp.mongodb.net:27017/Vtormetprom?ssl=true&replicaSet=atlas-lgm1bg-shard-0&authSource=admin&retryWrites=true&w=majority`
+const PORT = process.env.PORT || 3000
 
 const app = express()
-
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const Vtormetprom = new MongoVtormetrpom({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -19,14 +37,38 @@ app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  Vtormetprom
+}))
+app.use(csrf())
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
-const PORT = process.env.PORT || 3000
+async function start() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+start()
+
+
